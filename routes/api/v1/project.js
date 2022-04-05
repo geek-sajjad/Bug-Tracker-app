0533222de2ed9b3c2ef2efbb18dbed87f53283f0
geek-sajjad <sajjad.sohrabi2@gmail.com>
2022-04-05 15:54:25 +0430

    added jwt auth and .env.example

--- routes/api/v1/project.js
+++ routes/api/v1/project.js
@@ -4,12 +4,18 @@ const router = express.Router();
 const prisma = require('../../../utils/prisma');
 const validate = require('../../../middlewares/validator');
 const validationSchema = require('../../../validators/index');
+const url = require('url');
+
 router.get('/', async (req, res) => {
   try {
+    const queryObject = url.parse(req.url,true).query;
     const projects = await prisma.project.findMany({
       where: {
         ownerId: req.decoded.userId,
       },
+      include:{
+        issues: queryObject.include_issues === 'true' ? true: false
+      }
     });
     res.status(200).json(projects);
   } catch (error) {
@@ -17,40 +23,54 @@ router.get('/', async (req, res) => {
     res.json({ error: error.name });
   }
 });
-router.post('/',validate(validationSchema.createProject), async (req, res, next) => {
-  console.log("creating project");
-  try {
-    const { name ,status} = req.body;
-    const decoded = req.decoded;
-    const project = await prisma.project.create({
-      data: {
-        name: name,
-        status: status,
-        owner: {
-          connect: {
-            id: decoded.userId,
+router.post(
+  '/',
+  validate(validationSchema.createProject),
+  async (req, res, next) => {
+    console.log('creating project');
+    try {
+      const { name, status } = req.body;
+      const decoded = req.decoded;
+      const project = await prisma.project.create({
+        data: {
+          name: name,
+          status: status,
+          owner: {
+            connect: {
+              id: decoded.userId,
+            },
           },
         },
-      },
-    });
-    res.status(201).json(project);
-  } catch (error) {
-    console.log(error);
-    res.json({ error: error.name });
+      });
+      res.status(201).json(project);
+    } catch (error) {
+      console.log(error);
+      res.json({ error: error.name });
+    }
   }
-});
+);
 router.get('/:id', async (req, res) => {
   try {
     const id = +req.params.id;
-    const project = await prisma.project.findUnique({
+    const userId = req.decoded.userId;
+    const queryObject = url.parse(req.url,true).query;
+    const projects = await prisma.project.findMany({
       where: {
         id: id,
+        ownerId: userId,
+      },
+      include: {
+        issues: queryObject.include_issues === 'true' ? true: false
       },
     });
-    res.json(project);
+    if (projects.length === 0) {
+      return res.status(400).json({ message: 'project not found.' });
+    }
+
+    res.json(projects[0]);
   } catch (error) {
     console.log(error);
-    res.json({ error: error.name });
+    res.status(500).json({ error: error.name });
   }
 });
 
